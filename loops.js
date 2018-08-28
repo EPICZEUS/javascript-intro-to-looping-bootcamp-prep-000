a24c1919332825a6d45a23d09271137b642691c8
Sam Brozinsky <s77broz@aol.com>
2018-08-28 07:21:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,12 @@
 function forLoop(arr) {
   for (let i = 0; i < 25; i++) {
-    arr.push(`I am ${i} strange loop${i==1 ? "" : "s"}.`)
+    arr.push(`I am ${i} strange loop${i==1 ? "" : "s"}.`);
   }
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--);
+  }
+  return "done";
 }
